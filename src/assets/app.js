a2176e5207be6258c944b21ad229c7af0f2b33b4
Mariano Conchillo <marianoconchillo@hotmail.com>
2020-07-04 20:52:36 -0300

    navbar componentnavbar component added and background images configured

---
+++ src/assets/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  var urls = ['./assets/images/background1p.jpg', './assets/images/background2p.jpg', './assets/images/background3p.jpg'];
+
+  var cout = 1;
+  $('header').css('background-image', 'url("' + urls[0] + '")');
+  setInterval(function() {
+      setTimeout(500);
+    $('header').css('background-image', 'url("' + urls[cout] + '")');
+    cout == urls.length-1 ? cout = 0 : cout++;
+  }, 5000);
+
+});
