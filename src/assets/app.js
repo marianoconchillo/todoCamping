0a6e3f3de68e43c6004a1b3c7c335392089b9303
Mariano Conchillo <marianoconchillo@hotmail.com>
2020-07-08 14:32:15 -0300

    preload background images

--- src/assets/app.js
+++ src/assets/app.js
@@ -1,37 +1,10 @@
-
-// $(document).ready(function() {
-//   var urls = ['./assets/images/background1p.jpg', './assets/images/background2p.jpg', './assets/images/background3p.jpg'];
-//   var cout = 1;
-//   $('header').css('background-image', 'url("' + urls[0] + '")');
-//   setInterval(function() {
-//     $('header').css('background-image', 'url("' + urls[cout] + '")');
-//     cout == urls.length-1 ? cout = 0 : cout++;
-//   }, 5000);
-
-// });
-
-var timeWaiting = 1000;
-
 $(document).ready(function() {
   var urls = ['./assets/images/background1p.jpg', './assets/images/background2p.jpg', './assets/images/background3p.jpg'];
   var cout = 1;
-  var firstTime = true;
   $('header').css('background-image', 'url("' + urls[0] + '")');
   setInterval(function() {
-    if(firstTime){
-      $('html').css('background-image', 'url("' + urls[cout] + '")');   
-    } else {
-      $('header').css('background-image', 'url("' + urls[cout] + '")');
-    }
-    if(cout == urls.length-1){
-      timeWaiting = 5000;
-      firstTime = false;
-      cout = 0;
-      console.log(timeWaiting);
-    } else {
-      cout++;
-    }
-  }, timeWaiting);
-
+    $('header').css('background-image', 'url("' + urls[cout] + '")');
+    cout == urls.length-1 ? cout = 0 : cout++;
+  }, 5000);
 });
 
