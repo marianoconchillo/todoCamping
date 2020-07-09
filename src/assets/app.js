2a3064226d246a0d3eaeec669aac8abcdd9bb8eb
Mariano Conchillo <marianoconchillo@hotmail.com>
2020-07-08 23:08:01 -0300

    fix background errors

--- src/assets/app.js
+++ src/assets/app.js
@@ -1,10 +1,32 @@
+// $(document).ready(function() {
+//   var urls = ['./assets/images/background1p.jpg', './assets/images/background2p.jpg', './assets/images/background3p.jpg'];
+//   var cout = 1;
+//   $('header').css('background-image', 'url("' + urls[0] + '")');
+//   setInterval(function() {
+//     $('header').css('background-image', 'url("' + urls[cout] + '")');
+//     cout == urls.length-1 ? cout = 0 : cout++;
+//   }, 5000);
+// });
+
+// Wait 4 seconds to execute the function that changes the background image.
 $(document).ready(function() {
   var urls = ['./assets/images/background1p.jpg', './assets/images/background2p.jpg', './assets/images/background3p.jpg'];
   var cout = 1;
   $('header').css('background-image', 'url("' + urls[0] + '")');
-  setInterval(function() {
-    $('header').css('background-image', 'url("' + urls[cout] + '")');
-    cout == urls.length-1 ? cout = 0 : cout++;
-  }, 5000);
+  setTimeout(
+    setInterval(function() {
+      $('header').css('background-image', 'url("' + urls[cout] + '")');
+      cout == urls.length-1 ? cout = 0 : cout++;
+    }, 5000), 4000)
 });
 
+// Dissolve background image
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 500) {
+      $('.dissolve').css({'opacity' : '0'})
+    } else {
+      $('.dissolve').css({'opacity' : '1'})
+    }
+  })
+})
